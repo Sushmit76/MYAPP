832e0942175c464e51614e254369093da944dc87
sushmit srivastava <sushmits76btechcse2024@kccitm.edu.in>
2022-06-14 01:37:44 +0530

    jquery

--- myapp/app.js
+++ myapp/app.js
@@ -5,11 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/student');
-var teacherRouter = require('./routes/teacher')
-var loginRouter = require('./routes/login')
-
+var teacherRouter = require('./routes/teacher');
+var loginRouter = require('./routes/login');
+var idcardRouter= require('./routes/idcard');
 var app = express();
 
 // view engine setup
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
 app.use('/teacher', teacherRouter);
+// app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-//app.use('/users', usersRouter);
-
+app.use('/idcard', idcardRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
