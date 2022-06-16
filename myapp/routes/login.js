033dc19eca34832d13383ff7cd5b1b2fa43f3348
sushmit srivastava <sushmits76btechcse2024@kccitm.edu.in>
2022-06-17 01:42:48 +0530

    delete update

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,25 +1,98 @@
 var express = require('express');
 var router = express.Router();
+// var modelLogin = require("../model/login")
+var mysql = require('mysql');
+// const { getLoginPost } = require('../controller/controller');
+// const func = require('../model/login');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    
-    //res.json({name:"sushmit"})
-     res.render('login');
+var con = mysql.createConnection({
+  host: "easylearning.guru",
+  user: "kcc_student",
+  password: "Kccitm.edu.in1",
+  database: "kccStudent"
+});
+
+con.connect()
+
+router.get('/', function (req, res, next) {
+  console.log("Nitya");
+  con.connect(function (err) {
+    console.log("HARSH")
+    if (err) console.log("err");
+    console.log("Connected!");
+  });
+  // modelLogin.a(modelLogin.pandy.name)
+  console.log("himani")
+  console.log("SIR")
+  console.log("BITTU")
+  res.render('login')
+});
+
+router.get('/getLogin', function (req, res, next) {
+
+
+  con.query("select * from SUSHMIT", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render('login', { "data": result })
   });
 
-router.get('/getLogin', function(req, res, next) {
-    console.log(req.query)
-    res.send({name:"sushmit"})
-  //res.render('teacher')
-   
 });
-router.post('/getLogin', function(req, res, next) {
+
+router.post('/updateLogin', function (req, res, next) {
   console.log(req.body)
-  //res.send({name:"sushmit"})
-//res.render('teacher')
-res.json({"name":req.body.password})
- 
+  res.json({ "Name": "Hello" })
+});
+
+router.post('/deleteLogin', function (req, res, next) {
+  console.log(req.body.id)
+  // res.json({ "Name": "Hello" })
+  con.query("DELETE FROM SUSHMIT where id ="+req.body.id, function (err, result) {
+    if (err) res.json({code:0});
+    console.log(result);
+    res.json({code:1})
+  });
+});
+
+router.post('/getLogin', function (req, res, next) {
+  getLoginPost(req, res, next)
+  // console.log(req.body)
+  // res.send({"name":"sushmit"})
+  //res.render('teacher')
+  // var sql = "INSERT INTO `login` (`email`, `pass`) \
+  //   VALUES ('"+ req.body.email + "', '" + req.body.pass + "');"
+  // console.log(sql)
+  // con.connect()
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+
+  // });
+  // var sql1 = "select * from `login`"
+  // con.query(sql1, funcStion (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+
+  // });
+  // var t = con.query(sql);
+  // func.a("Utkrisht")
+  // func.natti("Abishake")
+  // a("Utkrrrisht")
+  //  console.log("Harsh loves Chocklet")
+  // res.json({ "Name": req.body })
+});
+
+router.all('/getPost', function (req, res, next) {
+  console.log(req.body)
+  if (req.body) {
+    //do something
+  } else {
+    //do something else
+  }
+  // res.send({"name":"sushmit"})
+  //res.render('teacher')
+  console.log(req.body.pass)
+  res.json({ "Name": req.body })
 });
 
 module.exports = router;
